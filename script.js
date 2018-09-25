9c3e2b1a11bfa521394df1c0abd4b42a299434ef
Dona Anda <ceka_dona@yahoo.com>
2018-09-24 20:41:46 -0700

    added handleclick functions

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ function initializeApp() {
 }
 
 function addClickHandlersToElements() {
+    $(".cartoon-character-add-form").on('click', '.btn-success', handleAddClick);
+    $(".cartoon-character-add-form").on('click', '.btn-default', handleCancelClick);
+    $(".cartoon-character-add-form").on('click', '.btn-primary', handleDataClick);
+}
+
+function handleAddClick() {
+
+}
+
+function handleCancelClick() {
+
+}
+
+function handleDataClick() {
 
 }
