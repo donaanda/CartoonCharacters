9da6b8b7c278a20398c2c3970fe21f565ec93297
Dona Anda <ceka_dona@yahoo.com>
2018-11-11 11:51:29 -0800

    implemented peform query

--- get_data.php
+++ get_data.php
@@ -1,4 +1,13 @@
 <?php
 
 require_once('mysql_credentials.php');
+
+$query = 'SELECT * FROM `cartoon_character`';
+
+$result = mysqli_query($connection, $query);
+
+$output = [
+    'success' => false,
+    'data' => []
+];
 ?>
